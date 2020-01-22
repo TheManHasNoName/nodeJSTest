5ef78af449fcd103987dd09522dda8a2429d1cbd
Elnas <xavierjohanis@gmail.com>
2020-01-23 05:16:06 +0800

    cleanup

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Posts');
 
-//GETS ALL POSTS
+//GET ALL POSTS
 router.get('/',  async (req, res) => {
     //res.send('This is a post request')
     try {
